4c4ab599e875dcacb4754d6061d5cdd97de85256
andmana <iam.andmana@gmail.com>
2025-03-28 00:02:13 +0700

    Add nav animaition

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 import NavigationLink from "../navigation/NavigationLink";
 import NavbarSearch from "../navbar-search/NavbarSearch";
@@ -8,8 +9,37 @@ import logoImg from "../../assets/images/logo.png";
 import iconCart from "../../assets/images/icon-cart.svg";
 
 const Navbar = () => {
+    const [scrollingDown, setScrollingDown] = useState(false);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+
+            // If scrolling down, hide the navbar; if scrolling up, show it
+            if (currentScrollY > lastScrollY) {
+                setScrollingDown(true); // Scrolling down
+            } else {
+                setScrollingDown(false); // Scrolling up
+            }
+
+            setLastScrollY(currentScrollY);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [lastScrollY]);
+
     return (
-        <motion.div className="fixed w-full">
+        <motion.div
+            className="fixed w-full"
+            initial={{ y: 0 }}
+            animate={{ y: scrollingDown ? -80 : 0 }} // Hide navbar when scrolling down
+            transition={{ type: "spring", stiffness: 300, damping: 30 }}
+        >
             <nav className="main-padding flex justify-between items-center gap-2.5">
                 <NavigationLink to={"/"}>
                     <img
