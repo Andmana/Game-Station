9e3fe89f9b7a57a98c4a7fb35e89c3bb6458846f
andmana <iam.andmana@gmail.com>
2025-03-28 19:29:17 +0700

    Make tilt hover on simple desc

--- src/components/contents/ContetFirst.tsx
+++ src/components/contents/ContetFirst.tsx
@@ -1,6 +1,9 @@
 import SectionWrapper from "./SectionWrapper";
 import backgroundVideo from "../../assets/videos/background-video.mp4";
 import QuickNavigation from "../quick-navigation/QuickNavigation";
+
+import { useMotionValue, useSpring, useTransform, motion } from "framer-motion";
+
 const ContentFirst = () => {
     return (
         <SectionWrapper>
@@ -16,35 +19,90 @@ const ContentFirst = () => {
                     ></video>
                 </div>
                 <QuickNavigation />
-
-                <div className="absolute z-10 w-full h-full main-padding flex justify-center items-end md:justify-end md:items-center">
-                    <div className="bg-[#2424247a] p-5 text-center mt-7 ">
-                        <h1 className="text-4xl font-black mb-4">
-                            GAME STATION
-                        </h1>
-                        <p className="max-w-[400px] w-auto">
-                            This isn't a real online game shop. You can't buy
-                            any games here.
-                        </p>
-
-                        <div className="flex justify-center gap-8 py-1 rounded-2xl text-center mt-2">
-                            <a
-                                className="w-[85px] border-b-2 px-7 py-2 flex justify-center"
-                                href="https://github.com/Andmana/Game-Station"
-                            >
-                                Repo
-                            </a>
-                            <a
-                                className="w-[85px] border-b-2 px-7 py-2 flex justify-center"
-                                href="https://rawg.io/apidocs"
-                            >
-                                RAWG API
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                <TiltCard />
             </section>
         </SectionWrapper>
+    );
+};
+
+const TiltCard = () => {
+    const x = useMotionValue(0);
+    const y = useMotionValue(0);
+
+    const mouseXSpring = useSpring(x);
+    const mouseYSpring = useSpring(y);
+
+    const rotateX = useTransform(
+        mouseYSpring,
+        [-0.5, 0.5],
+        ["10.5deg", "-10.5deg"]
+    );
+
+    const rotateY = useTransform(
+        mouseXSpring,
+        [-0.5, 0.5],
+        ["-10.5deg", "10.5deg"]
+    );
+
+    const handleMouseMove: React.MouseEventHandler<HTMLDivElement> = (
+        event
+    ) => {
+        const rect = event.currentTarget.getBoundingClientRect();
+        const { width, height } = rect;
+
+        const mouseX = event.clientX - rect.left;
+        const mouseY = event.clientY - rect.top;
+
+        const xPct = mouseX / width - 0.5;
+        const yPct = mouseY / height - 0.5;
+
+        x.set(xPct);
+        y.set(yPct);
+    };
+
+    const handleMouseLeave = () => {
+        x.set(0);
+        y.set(0);
+    };
+
+    return (
+        <div className="transform-3d absolute z-10 w-full h-full main-padding flex justify-center items-end md:justify-end md:items-center">
+            <motion.div
+                className="bg-[#00000066] mt-7"
+                onMouseMove={handleMouseMove}
+                onMouseLeave={handleMouseLeave}
+                style={{ rotateX, rotateY, transformStyle: "preserve-3d" }}
+            >
+                <div
+                    style={{
+                        transform: "translateZ(30px)",
+                        transformStyle: "preserve-3d",
+                    }}
+                    className=" bg-[#2424247a] p-5 text-center  inset-6"
+                >
+                    <h1 className="text-4xl font-black mb-4">GAME STATION</h1>
+                    <p className="max-w-[400px] w-auto">
+                        This isn't a real online game shop. You can't buy any
+                        games here.
+                    </p>
+
+                    <div className="flex justify-center gap-8 py-1 rounded-2xl text-center mt-2">
+                        <a
+                            className="w-[100px] border-b-2  py-2 flex justify-center"
+                            href="https://github.com/Andmana/Game-Station"
+                        >
+                            Repo
+                        </a>
+                        <a
+                            className="w-[100px] border-b-2  py-2 flex justify-center"
+                            href="https://rawg.io/apidocs"
+                        >
+                            RAWG API
+                        </a>
+                    </div>
+                </div>
+            </motion.div>
+        </div>
     );
 };
 
