df47dd0751dcdb20662bb3b354e8b2d349637c49
andmana <iam.andmana@gmail.com>
2025-03-28 23:43:55 +0700

    Change offset scroll hook

--- src/components/contents/ContentSecond.tsx
+++ src/components/contents/ContentSecond.tsx
@@ -8,19 +8,20 @@ const ContentSecond = () => {
 
     const { scrollYProgress } = useScroll({
         target: containerRef,
-        offset: ["end end", "start start"],
+        offset: ["start end", "end start"], // This starts the animation when the top of the container reaches the bottom of the viewport, and ends when the bottom reaches the top.
     });
 
+    // Rotate transformations
     const rotateXContainer = useTransform(
         scrollYProgress,
         [0, 0.25, 0.5, 0.75, 1],
-        ["80deg", "50deg", "20deg", "-20deg", "-50deg"]
+        ["80deg", "50deg", "20deg", "50deg", "80deg"]
     );
 
     const rotateXContent = useTransform(
         scrollYProgress,
         [0, 0.25, 0.5, 0.75, 1],
-        ["70deg", "35deg", "5deg", "45deg", "-70deg"]
+        ["70deg", "35deg", "5deg", "35deg", "70deg"]
     );
 
     const opacity = useTransform(
@@ -38,12 +39,12 @@ const ContentSecond = () => {
                 <motion.div
                     ref={containerRef}
                     style={{ rotateX: rotateXContainer }}
-                    className="absolute w-8/12 h-8/12 bg-amber-400 trasnform origin-bottom "
+                    className="absolute w-11/12 h-8/12 md:w-9/12 md:h-9/12 bg-amber-400 transform origin-bottom"
                 ></motion.div>
                 <motion.div
                     ref={contentRef}
                     style={{ rotateX: rotateXContent, opacity }}
-                    className="absolute w-8/12 h-8/12 bg-red-400 trasnform origin-bottom translate-z-2"
+                    className="absolute w-11/12 h-8/12 md:w-9/12 md:h-9/12 bg-red-400 transform origin-bottom translate-z-3"
                 ></motion.div>
             </div>
         </SectionWrapper>
