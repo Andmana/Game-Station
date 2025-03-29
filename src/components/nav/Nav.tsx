7e776590a3e644b1f0dfdcfe5e443fbb7a9aecfc
andmana <iam.andmana@gmail.com>
2025-03-29 22:08:27 +0700

    Better Nav
    Better Nav

---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,64 @@
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
+import { useState } from "react";
+
+import CLink from "../common/CLink";
+import CButton from "../common/CButton";
+
+import logoImg from "../../assets/images/logo.png";
+import iconCart from "../../assets/images/icon-cart.svg";
+import NavSearch from "./NavSearch";
+
+const Nav = () => {
+    const { scrollY } = useScroll();
+    const [hidden, setHidden] = useState(false);
+    const [prevScroll, setPrevScroll] = useState(0);
+
+    function update(latest: number, prev: number): void {
+        if (latest < prev) {
+            setHidden(false);
+        } else if (latest > 100 && latest > prev) {
+            setHidden(true);
+        }
+    }
+
+    useMotionValueEvent(scrollY, "change", (latest: number) => {
+        update(latest, prevScroll);
+        setPrevScroll(latest);
+    });
+
+    return (
+        <motion.nav
+            animate={hidden ? { opacity: 0, y: "-2rem" } : { opacity: 1, y: 0 }}
+            transition={{
+                ease: [0.1, 0.25, 0.3, 1],
+                duration: 0.6,
+                staggerChildren: 0.05,
+            }}
+            className="fixed z-100 isolate w-full top-0 left-0 main-padding nav-shadow flex justify-between items-center gap-2.5"
+        >
+            {/* Hero */}
+            <CLink to="/">
+                <img
+                    src={logoImg}
+                    className="w-8 h-8"
+                    aria-hidden="true"
+                    alt=""
+                />
+                <span className="text-xl md:text-2xl font-bold w-0 h-0 sm:h-fit sm:w-fit overflow-hidden">
+                    GAME STATION
+                </span>
+            </CLink>
+
+            {/* Game Search */}
+            <NavSearch />
+
+            {/* Cart */}
+
+            <CButton>
+                <img className="w-8 h-8" src={iconCart} alt="submit search" />
+            </CButton>
+        </motion.nav>
+    );
+};
+
+export default Nav;
---
+++ src/components/nav/NavSearch.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+import iconSearch from "../../assets/images/icon-search.svg";
+import { transitionConfig } from "../common/modules";
+
+const NavSearch = () => {
+    const [isFocused, setIsFocused] = useState(false);
+
+    return (
+        <div
+            className={`flex-1 max-w-[500px] flex items-center justify-center`}
+        >
+            <motion.div
+                className="flex items-center"
+                initial={{ width: "200px" }}
+                animate={{
+                    width: isFocused ? "clamp(200px, 90%, 450px)" : "200px",
+                }}
+                transition={{
+                    duration: 0.15,
+                    type: "spring",
+                    damping: 20,
+                    stiffness: 300,
+                }}
+            >
+                <input
+                    type="text"
+                    className="py-0.5 px-2 rounded-md bg-white text-black w-full focus:outline-none"
+                    placeholder="Search games..."
+                    onFocus={() => setIsFocused(true)}
+                    onBlur={() => setIsFocused(false)}
+                />
+
+                <button className="w-6 h-6 absolute right-2 z-10">
+                    <motion.img
+                        {...transitionConfig}
+                        className="w-6 h-6"
+                        src={iconSearch}
+                        alt="submit search"
+                    />
+                </button>
+            </motion.div>
+        </div>
+    );
+};
+
+export default NavSearch;
