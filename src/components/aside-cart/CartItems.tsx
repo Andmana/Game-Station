24d4d1a1556d1b17607986bb84438b35c8564eed
andmana <iam.andmana@gmail.com>
2025-04-10 20:33:58 +0700

    Fix redirect to game page when remove cart item

--- src/components/aside-cart/CartItems.tsx
+++ src/components/aside-cart/CartItems.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
 
-import CButton from "../common/CButton";
 import closeImage from "../../assets/images/icon-close.svg";
 
 import { SPRING_OPTIONS } from "../common/modules";
 import { IGame } from "../../types/IGame";
+import { MouseEvent } from "react";
 
 interface CartItemsProps {
     cartItems: IGame[];
@@ -13,6 +13,10 @@ interface CartItemsProps {
 }
 
 const CartItems = ({ cartItems, removeCartItem }: CartItemsProps) => {
+    const handleRemoveItem = (event: MouseEvent, id: number) => {
+        event.preventDefault();
+        removeCartItem(id);
+    };
     return (
         <motion.div
             className="w-full flex-1 flex flex-col gap-5 overflow-scroll"
@@ -29,17 +33,19 @@ const CartItems = ({ cartItems, removeCartItem }: CartItemsProps) => {
                             to={`/game/${item.id}`}
                             className="relative w-full bg-[#242424] py-5 px-4 flex justify-between items-center gap-2 rounded-xl"
                         >
-                            <div className="absolute top-2 right-2 h-6 w-6 p-1.5 bg-black rounded-[50%]">
-                                <CButton
-                                    onClick={() => removeCartItem(item.id)}
-                                >
-                                    <img
-                                        src={closeImage}
-                                        alt=""
-                                        aria-hidden="true"
-                                    ></img>
-                                </CButton>
-                            </div>
+                            <motion.button
+                                className="absolute top-2 right-2 h-6 w-6 p-1.5 bg-[#d1d1d157] rounded-[50%] flex justify-center items-center"
+                                whileHover={{ scale: 1.2 }}
+                                onClick={(event: MouseEvent) =>
+                                    handleRemoveItem(event, item.id)
+                                }
+                            >
+                                <img
+                                    src={closeImage}
+                                    alt=""
+                                    aria-hidden="true"
+                                ></img>
+                            </motion.button>
                             <div
                                 className="w-[80px] h-[100px] rounded-md bg-center bg-cover"
                                 style={{
