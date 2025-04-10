abb978465c273ed9e6fe663f1b94b3cda50050dd
andmana <iam.andmana@gmail.com>
2025-04-10 15:10:50 +0700

    Clean

--- src/components/carousel/CarouselV2.tsx
+++ src/components/carousel/CarouselV2.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import img1 from "../../assets/images/1.jpg";
 import img2 from "../../assets/images/2.jpg";
 import img3 from "../../assets/images/3.jpg";
